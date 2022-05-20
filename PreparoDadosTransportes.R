f1a84f1b4aad46a3674f67117f72784c76fd8202
ex0ticOne <rricardorb@gmail.com>
2022-05-20 18:09:01 -0300

    atualizaçoes

--- PreparoDadosTransportes.R
+++ PreparoDadosTransportes.R
@@ -2,31 +2,76 @@ library(prophet)
 library(dplyr)
 library(htmltools)
 library(tidyr)
+library(readr)
 library(WriteXLS)
 
 argumentos <- commandArgs(trailingOnly = TRUE)
 
-download.file('http://www2.susep.gov.br/redarq.asp?arq=BaseCompleta%2ezip', 
-              destfile = 'BaseCompleta.zip')
+#download.file('http://www2.susep.gov.br/redarq.asp?arq=BaseCompleta%2ezip', 
+             # destfile = 'BaseCompleta.zip')
 
-unzip('BaseCompleta.zip', files = c('SES_UF2.csv', 'Ses_cias.csv'))
+#unzip('BaseCompleta.zip', files = c('SES_UF2.csv', 'Ses_cias.csv'))
 
 dataset <- read.csv('SES_UF2.csv', sep = ";", dec = ",")
-companhias <- read.csv('Ses_cias.csv', sep = ";", dec = ",")
-dataset$ramos <- as.integer(dataset$ramos)
+dataset <- dataset[order(dataset$damesano, decreasing = FALSE), ]
 
-ramos_transportes <- c("621", "622")
+dataset$UF <- toupper(dataset$UF)
+dataset$damesano <- as.Date(paste0(substring(dataset$damesano, 0, 4), "-", 
+                                   substring(dataset$damesano, 5, 7), "-01"))
 
-dataset_transportes <- dataset[dataset$ramos %in% ramos_transportes, ]
+companhias <- read_delim("Ses_cias.csv", 
+                         delim = ";", escape_double = FALSE, col_types = cols(Coenti = col_integer()), 
+                         locale = locale(encoding = "ISO-8859-1"), 
+                         trim_ws = TRUE)
 
-#Remoção das companhias que não trabalham com transporte
-dataset_transportes <- dataset_transportes[dataset_transportes$sin_dir > 0, ]
+dataset_transportes <- dataset %>% 
+  group_by(damesano, coenti, UF) %>%
+  summarise(PremiosDiretos = sum(premio_dir), 
+            SinistrosDiretos = sum(sin_dir)) %>%
+  mutate(Sinistralidade = SinistrosDiretos / PremiosDiretos)
 
-#Join com a tabela das companhias
-dataset_transportes <- left_join(dataset_transportes, companhias[, c(1:2)], by = c('coenti' = 'Coenti'), )
+dataset_transportes <- left_join(dataset_transportes, companhias[, c(1:2)], by = c('coenti' = 'Coenti'))
 
-#
-dataset_transportes <- dataset_transportes[, c(1:5, 7, 12)]
+lista_seguradoras <- data.frame(table(dataset_transportes$Noenti))
+lista_seguradoras <- lista_seguradoras$fr
 
-#Índice de sinistralidade
-dataset_transportes$sinistralidade <- round(as.double(dataset_transportes$sin_dir / dataset_transportes$premio_dir), digits = 2)
+lista_UF <- as.factor(dataset_transportes$UF)
+
+for (seguradora in lista_seguradoras) {
+    
+    for (uf in lista_UF) {
+    
+    if (length(dataset_transportes$y[dataset_transportes$Noenti == seguradora &
+                                   dataset_transportes$ramos == ramo_transporte &
+                                   dataset_transportes$UF == uf]) < 2) {
+                                     
+      next
+      
+    } else {  
+    
+    dataset_treino <- dataset_transportes[dataset_transportes$Noenti == seguradora &
+                                          dataset_transportes$ramos == ramo_transporte &
+                                          dataset_transportes$UF == uf, c("ds", "y")]  
+    
+    modelo <- prophet(dataset_treino, 
+                      growth = "linear", 
+                      yearly.seasonality = TRUE, 
+                      weekly.seasonality = FALSE, 
+                      daily.seasonality = FALSE, 
+                      seasonality.mode = "multiplicative")
+    
+    periodos_futuros <- make_future_dataframe(modelo, 48, 
+                                              freq = "month", include_history = TRUE)
+    
+    projecao <- predict(modelo, periodos_futuros)
+    
+    plot <- dyplot.prophet(modelo, projecao, 
+                           main = paste0("Projeção Sinistralidade - ", seguradora, " - ", ramo_transporte, " - ", uf), 
+                           xlab = "Linha Temporal", 
+                           ylab = "Sinistralidade")
+    
+    save_html(plot, paste0("PROJECOES/Projeção Sinistralidade - ", seguradora, " - ", ramo_transporte, " - ", uf, ".html"), background = "antiquewhite")
+      
+  }
+    }
+  }

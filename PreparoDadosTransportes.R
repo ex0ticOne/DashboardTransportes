28d2d6d8138fec4b87d041af40685a6f6ac42ae7
ex0ticOne <rricardorb@gmail.com>
2022-05-22 09:54:08 -0300

    Ajuste loop

--- PreparoDadosTransportes.R
+++ PreparoDadosTransportes.R
@@ -7,12 +7,15 @@ library(WriteXLS)
 
 argumentos <- commandArgs(trailingOnly = TRUE)
 
-ano_escolhido <- as.integer(argumentos[1])
-ramo_escolhido <- case_when(argumentos[2] == 1 ~ 621,
-                            argumentos[2] == 2 ~ 622)
+#ano_escolhido <- as.integer(argumentos[1])
+ano_escolhido <- 2010
 
-seguradoras_escolhidas <- read.csv('seguradoras_escolhidas.csv')
-companhias_escolhida <- seguradoras_escolhidas$Noenti[seguradoras_escolhidas$escolhida == 1]
+companhias <- read_delim("lista_seguradoras.csv", 
+                         delim = ",", escape_double = FALSE, col_types = cols(Coenti = col_integer()), 
+                         locale = locale(encoding = "ISO-8859-1"), 
+                         trim_ws = TRUE)
+
+companhias_escolhidas <- as.vector(companhias$Coenti[companhias$escolhida == 1])
 
 #download.file('http://www2.susep.gov.br/redarq.asp?arq=BaseCompleta%2ezip', 
          #     destfile = 'BaseCompleta.zip')
@@ -20,18 +23,12 @@ companhias_escolhida <- seguradoras_escolhidas$Noenti[seguradoras_escolhidas$esc
 unzip('BaseCompleta.zip', files = c('SES_UF2.csv', 'Ses_cias.csv'))
 
 dataset <- read.csv('SES_UF2.csv', sep = ";", dec = ",")
-dataset <- dataset[order(dataset$damesano, decreasing = FALSE), ]
 
 dataset <- dataset[dataset$damesano >= paste0(ano_escolhido, "01"), ]
 
 ramos_transporte <- c("621", "622")
 
 dataset$UF <- toupper(dataset$UF)
-
-companhias <- read_delim("Ses_cias.csv", 
-                         delim = ";", escape_double = FALSE, col_types = cols(Coenti = col_integer()), 
-                         locale = locale(encoding = "ISO-8859-1"), 
-                         trim_ws = TRUE)
 
 dataset_transportes <- dataset %>%
   mutate(ds = paste0(substring(dataset$damesano, 0, 4), "-", substring(dataset$damesano, 5, 7), "-01")) %>%
@@ -44,25 +41,28 @@ dataset_transportes <- dataset %>%
   filter(y > 0) %>%
   left_join(y = companhias[, c(1:2)], by = c('coenti' = 'Coenti'))
 
-lista_seguradoras <- data.frame(table(dataset_transportes$Noenti))
-lista_seguradoras <- as.factor(lista_seguradoras$Var1[lista_seguradoras$Freq != ""])
+lista_UF <- data.frame(table(dataset_transportes$UF))
+lista_UF <- as.factor(lista_UF$Var1[lista_UF$Freq != 0])
 
-lista_UF <- as.factor(dataset_transportes$UF)
+lista_seguradoras <- data.frame(table(dataset_transportes$coenti))
+lista_seguradoras <- as.factor(lista_seguradoras$Var1[lista_seguradoras$Freq != 0])
 
-for (seguradora in companhias_escolhida) {
+for (seguradora in lista_seguradoras) {
+  
+  for (ramo in ramos_transporte) {
   
     for (uf in lista_UF) {
-    
-    if (length(dataset_transportes$y[dataset_transportes$Noenti == seguradora &
-                                   dataset_transportes$ramos == ramo_escolhido &
-                                   dataset_transportes$UF == uf]) < 2) {
-                                     
-      next
       
-    } else {  
+      if (length(dataset_transportes$Noenti[dataset_transportes$coenti == seguradora &
+                                     dataset_transportes$ramos == ramo &
+                                     dataset_transportes$UF == uf]) < 2) {
+        
+        next
+        
+      } else {
     
-    dataset_treino <- dataset_transportes[dataset_transportes$Noenti == seguradora &
-                                          dataset_transportes$ramos == ramo_escolhido &
+    dataset_treino <- dataset_transportes[dataset_transportes$coenti == seguradora &
+                                          dataset_transportes$ramos == ramo &
                                           dataset_transportes$UF == uf, c("ds", "y")]  
     
     modelo <- prophet(dataset_treino, 
@@ -78,13 +78,14 @@ for (seguradora in companhias_escolhida) {
     projecao <- predict(modelo, periodos_futuros)
     
     plot <- dyplot.prophet(modelo, projecao, 
-                           main = paste0("Projeção Sinistralidade - ", seguradora, " - ", ramo_escolhido, " - ", uf), 
+                           main = paste0("Projeção Sinistralidade - ", companhias$Noenti[companhias$Coenti == seguradora], " - ", ramo, " - ", uf), 
                            xlab = "Linha Temporal", 
                            ylab = "Sinistralidade")
     
-    save_html(plot, paste0("PROJECOES/Projeção Sinistralidade - ", seguradora, " - ", ramo_escolhido, " - ", uf, ".html"), 
+    save_html(plot, paste0("PROJECOES/Projeção Sinistralidade - ", companhias$Noenti[companhias$Coenti == seguradora], " - ", ramo, " - ", uf, ".html"), 
               background = "antiquewhite")
       
   }
-    }
+}
   }
+}
